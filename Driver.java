c2063b9d82d1f332bdda82e3e3464f769e0c0027
gTopa <gtopa@stuy.edu>
2015-12-12 14:47:04 -0500

    Quest Complete

--- Driver.java
+++ Driver.java
@@ -17,24 +17,24 @@ public class Driver{
     }
       
 
-      /* //UNCOMMENT THIS WHEN YOU DO Volume
-      public static Volume makeRandomVolumeShape(){
-      String name = ""+letter()+"-3d";
-      double value = Math.round(Math.random()*10.0);
-      double choice = Math.random();
-      if(choice < 0.33){
-      return new Sphere(value,name);
-      }else{
-      double value2 = Math.round(Math.random()*10.0);
-      if(choice < 0.66){
-      return new Cylinder(value,value2,name);
-      }else{
-      double value3 = Math.round(Math.random()*10.0);
-      return new Prism(value,value2,value3,name);
-      }
-      }
-      }
-    */
+    //UNCOMMENT THIS WHEN YOU DO Volume
+    public static Volume makeRandomVolumeShape(){
+	String name = ""+letter()+"-3d";
+	double value = Math.round(Math.random()*10.0);
+	double choice = Math.random();
+	if(choice < 0.33){
+	    return new Sphere(value,name);
+	}else{
+	    double value2 = Math.round(Math.random()*10.0);
+	    if(choice < 0.66){
+		return new Cylinder(value,value2,name);
+	    }else{
+		double value3 = Math.round(Math.random()*10.0);
+		return new Prism(value,value2,value3,name);
+	    }
+	}
+    }
+    
 
     public static void main(String[]args){
 	
@@ -50,17 +50,17 @@ public class Driver{
 	}
 	
 
-	/*//UNCOMMENT THIS WHEN YOU DO VOLUME
-	  Volume[] threes = new Volume[6];
-	  for(int i = 0; i < threes.length; i++){
-	  threes[i]=makeRandomVolumeShape();
-	  }
-	  System.out.println("\nVolume tests:");
-	  for(Volume v : threes){
-	  System.out.println(v);
-	  System.out.println(v.getVolume());
-	  }
-	*/
+	//UNCOMMENT THIS WHEN YOU DO VOLUME
+	Volume[] threes = new Volume[6];
+	for(int i = 0; i < threes.length; i++){
+	    threes[i]=makeRandomVolumeShape();
+	}
+	System.out.println("\nVolume tests:");
+	for(Volume v : threes){
+	    System.out.println(v);
+	    System.out.println(v.getVolume());
+	}
+	
     }
 }
 
--- Sphere.java
+++ Sphere.java
@@ -1,10 +1,13 @@
-public class Sphere extends Circle{
+public class Sphere extends Circle implements Volume{
 
     public Sphere(double radius, String name){
 	super(radius,name);
     }
 
     public String toString(){
-	return "FIXME!";
+	return "Sphere "+getName()+" with radius "+getRadius();
+    }
+    public double getVolume(){
+	return (4/3.0)*Math.PI*Math.pow(getRadius(),3);
     }
 }
